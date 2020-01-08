6ca7db91fdec2910d7e69bc7f859d23bb0634389
Chun Ly <astro.chun@gmail.com>
2020-01-08 15:56:56 -0700

    tests.ldap_connect: docstring for LDAP Connection

--- tests/ldap_connection.py
+++ tests/ldap_connection.py
@@ -4,7 +4,19 @@ import logging
 logger = logging.getLogger( __name__ )
 
 class LDAPConnection( object ):
-    
+    '''
+
+    Quick how to:
+
+    from DataRepository_patrons.tests import ldap_connection
+    eds_hostname = 'eds.arizona.edu'
+    ldap_base_dn = 'dc=eds,dc=arizona,dc=edu'
+    ldc = ldap_connection.LDAPConnection(eds_hostname, ldap_base_dn, USERNAME, PASSWORD)
+
+    ldap_query = '(& (employeePrimaryDept=0404) )'
+    ldc.ldc.search(ldap_search_dn, ldap_query, attributes = ldap_attribs )
+    '''
+
     def __init__( self, ldap_host, ldap_base_dn, ldap_user, ldap_passwd):
         logger.debug( 'entered' )
         
