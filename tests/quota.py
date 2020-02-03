8209bf366b4a266eac4be138bfbbc97dcfa77fc4
Chun Ly <astro.chun@gmail.com>
2020-02-03 10:46:42 -0700

    quota: Minor PEP8 formatting (iss #6); Additional docstrings info

--- tests/quota.py
+++ tests/quota.py
@@ -1,21 +1,23 @@
 from .ldap_connection import ual_grouper_base
 
+
 def ual_ldap_quota_query(ual_class):
-    '''
-    Construct RFC 4512-compatible LDAP query to search for those within
-    a UAL-based classification patron group to set default quota
+    """
+    Purpose:
+      Construct RFC 4512-compatible LDAP query to search for those within
+      a UAL-based classification patron group to set default quota
 
     :param ual_class: A string to indicate types. Options are:
-    'faculty'   (for faculty and staff)
-    'grad'      (for grad students)
-    'undergrad' (for undergraduate)
+      'faculty'   (for faculty and staff)
+      'grad'      (for grad students)
+      'undergrad' (for undergraduate)
 
     :return ldap_query: list containing a single query string
-    '''
+    """
 
     if ual_class == 'faculty':
-        ldap_query = '( | ({}) '.format(ual_grouper_base('ual-faculty'))+\
-                     '({}) '.format(ual_grouper_base('ual-staff'))+\
+        ldap_query = '( | ({}) '.format(ual_grouper_base('ual-faculty')) + \
+                     '({}) '.format(ual_grouper_base('ual-staff')) + \
                      '({}) )'.format(ual_grouper_base('ual-dcc'))
 
     if ual_class == 'grad':
