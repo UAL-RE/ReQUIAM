82e85fda8f2dd51542de33a445e1a8845f7e74d5
Chun Ly <astro.chun@gmail.com>
2020-02-05 10:54:37 -0700

    grouper_query.GrouperQuery: Update docstrings (iss #7)

--- tests/grouper_query.py
+++ tests/grouper_query.py
@@ -3,19 +3,21 @@ import requests
 
 class GrouperQuery(object):
     """
-    This class initializes an HTTP request to retrieve Grouper membership.
-    This code was adapted from the following repository:
-       https://github.com/ualibraries/patron-groups
+    Purpose:
+      This class initializes an HTTP request to retrieve Grouper membership.
+      This code was adapted from the following repository:
+         https://github.com/ualibraries/patron-groups
 
-    Quick how to:
+    Usage:
+      Quick how to:
+        from DataRepository_patrons.tests import grouper_query
+        grouper_host = 'grouper.iam.arizona.edu'
+        grouper_base_path = 'grouper-ws/servicesRest/json/v2_2_001/groups'
+        grouper_group = 'arizona.edu:dept:LBRY:figshare:portal:sci_math'
+        gq = grouper_query.GrouperQuery(grouper_host, grouper_base_path, USERNAME, PASSWORD, grouper_group)
 
-    from DataRepository_patrons.tests import grouper_query
-    grouper_host = 'grouper.iam.arizona.edu'
-    grouper_base_path = 'grouper-ws/servicesRest/json/v2_2_001/groups'
-    gq = GrouperQuery(grouper_host, grouper_base_path, USERNAME, PASSWORD, grouper_group)
-
-    # You can retrieve the uaid via:
-    members = gq.members
+      You can retrieve the EDS uid via:
+        members = gq.members
     """
 
     def __init__(self, grouper_host, grouper_base_path, grouper_user, grouper_password, grouper_group):
