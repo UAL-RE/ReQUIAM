0f243c4539ee4a15b2069b99e352943bbc99e84d
Chun Ly <astro.chun@gmail.com>
2020-02-05 10:57:10 -0700

    grouper_query.figshare_stem: Update docstrings (iss #7)

--- tests/grouper_query.py
+++ tests/grouper_query.py
@@ -55,6 +55,13 @@ def figshare_stem(stem):
        Options are: 'quota', 'portal'
 
     :return stem_query: str
+
+    Usage:
+      For quota stem, call as: figshare_stem('quota')
+        > 'arizona.edu:dept:LBRY:figshare:quota'
+
+      For portal stem, call as: figshare_stem('portal')
+        > 'arizona.edu:dept:LBRY:figshare:portal'
     """
 
     stem_query = 'arizona.edu:dept:LBRY:figshare:{}'.format(stem)
