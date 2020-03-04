7abba0639f57badfb017d849331674fbb11e5a5e
Chun Ly <astro.chun@gmail.com>
2020-03-04 15:53:17 -0700

    Add setup.py script (iss #16)

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='DataRepository_patrons',
+    version='v0.6.0',
+    packages=find_packages('DataRepository_patrons'),
+    url='https://github.com/ualibraries/DataRepository_patrons',
+    license='MIT License',
+    author='Chun Ly',
+    author_email='astro.chun@gmail.com',
+    description='Query EDS information to set EDS attributes specific for Figshare account management',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
