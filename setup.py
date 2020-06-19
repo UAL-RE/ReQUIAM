1f467a3e41e1a58a4419db2837797ad8c9787f1d
Chun Ly <astro.chun@gmail.com>
2020-06-19 09:21:56 -0700

    Bump version: v0.9.0 -> v0.9.1 (iss #6)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='requiam',
-    version='v0.9.0',
+    version='v0.9.1',
     packages=find_packages('requiam'),
     url='https://github.com/ualibraries/ReQUIAM',
     license='MIT License',
