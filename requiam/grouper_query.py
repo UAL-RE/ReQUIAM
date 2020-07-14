f899bd049da2bf92036728be6a43d6863e981a15
Chun Ly <astro.chun@gmail.com>
2020-07-14 14:27:00 -0700

    Refactor figshare_stem to commons module #31

--- requiam/grouper_query.py
+++ requiam/grouper_query.py
@@ -1,5 +1,7 @@
 import requests
 
+from .commons import figshare_stem
+
 
 class GrouperQuery(object):
     """
@@ -45,28 +47,6 @@ class GrouperQuery(object):
     @property
     def members(self):
         return set(self._members)
-
-
-def figshare_stem(stem):
-    """
-    Purpose:
-      Construct Grouper figshare stems
-
-    :param stem: string corresponding to the sub-stem
-       Options are: 'quota', 'portal'
-
-    :return stem_query: str
-
-    Usage:
-      For quota stem, call as: figshare_stem('quota')
-        > 'arizona.edu:dept:LBRY:figshare:quota'
-
-      For portal stem, call as: figshare_stem('portal')
-        > 'arizona.edu:dept:LBRY:figshare:portal'
-    """
-
-    stem_query = 'arizona.edu:dept:LBRY:figshare:{}'.format(stem)
-    return stem_query
 
 
 def figshare_group(group, root_stem):
