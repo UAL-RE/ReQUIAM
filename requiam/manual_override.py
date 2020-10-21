3113ad96f1e6fc1ec96a9cd604a5f83450ccfb68
Chun Ly <astro.chun@gmail.com>
2020-10-21 16:03:35 -0700

    manual_override: Fix bug with set handling #85

--- requiam/manual_override.py
+++ requiam/manual_override.py
@@ -204,11 +204,14 @@ def update_entries(ldap_set, netid, uaid, action, log=None):
 
     new_ldap_set = set(ldap_set)
 
+    netid = list(netid)
+    uaid  = list(uaid)
+
     if action == 'remove':
         remove_uaid = ldap_set & set(uaid)
         if len(remove_uaid) > 0:
             remove_netid = [netid[i] for i in range(len(netid)) if
-                            uaid[i] in remove_uaid]
+                            uaid[i] in list(remove_uaid)]
             log.info(f"Removing : {', '.join(remove_netid)}")
         else:
             log.info("Nothing is removed")
@@ -218,7 +221,7 @@ def update_entries(ldap_set, netid, uaid, action, log=None):
         new_uaid = set(uaid) - ldap_set
         if len(new_uaid) > 0:
             new_netid = [netid[i] for i in range(len(netid)) if
-                         uaid[i] in new_uaid]
+                         uaid[i] in list(new_uaid)]
             log.info(f"Adding : {', '.join(new_netid)}")
         else:
             log.info(f"Nothing is added")
