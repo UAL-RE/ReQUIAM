2b9cebd5ca7d90fadc453928cf840531eb8c05bb
Chun Ly <astro.chun@gmail.com>
2020-11-10 15:50:56 -0700

    test_commons: Add `test_figshare_stem` #91

--- tests/test_commons.py
+++ tests/test_commons.py
@@ -1,5 +1,10 @@
 from requiam import TimerClass
+from requiam.commons import figshare_stem
+
 from datetime import datetime, timedelta
+
+prod_stem = 'arizona.edu:dept:LBRY:figshare'
+stage_stem = 'arizona.edu:dept:LBRY:figtest'
 
 
 def test_TimerClass():
@@ -19,3 +24,20 @@ def test_TimerClass():
     assert isinstance(tc.stop, datetime)
     assert isinstance(tc.delta, timedelta)
     assert isinstance(tc.format, str)
+
+
+def test_figshare_stem():
+
+    assert figshare_stem(production=False) == stage_stem
+    assert figshare_stem(production=True) == prod_stem
+
+    for stem in ['quota', 'portal']:
+        assert figshare_stem(stem, production=True) == \
+               f"{prod_stem}:{stem}"
+        assert figshare_stem(stem, production=False) == \
+               f"{stage_stem}:{stem}"
+
+        assert figshare_stem(stem=stem, production=True) == \
+               f"{prod_stem}:{stem}"
+        assert figshare_stem(stem=stem, production=False) == \
+               f"{stage_stem}:{stem}"
