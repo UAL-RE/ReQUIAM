7c72fddc8f4c8c4433e68e0252a90458c7dd3919
Chun Ly <astro.chun@gmail.com>
2021-03-02 13:57:04 -0700

    Fix log_dir path for logger unit tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,6 +1,7 @@
 from requiam import logger, git_info
 
 from os.path import join
+from os import getcwd
 import logging
 from datetime import date
 
@@ -8,13 +9,12 @@ import configparser
 import pandas as pd
 
 today = date.today()
-log_dir = ''
 logfile = f'testlog.{today.strftime("%Y-%m-%d")}.log'
 
 
 def test_LogClass():
 
-    log0 = logger.LogClass(log_dir, logfile).get_logger()
+    log0 = logger.LogClass(getcwd(), logfile).get_logger()
 
     log0.info("Print INFO test")
     log0.debug("Print DEBUG test")
@@ -44,9 +44,9 @@ def test_get_hostname():
 
 
 def test_log_setup_LogCommons():
-    logfile_prefix = 'test'
+    logfile_prefix = 'testlog'
 
-    log0, log_filename = logger.log_setup('temp/', logfile_prefix)
+    log0, log_filename = logger.log_setup(getcwd(), logfile_prefix)
 
     gi = git_info.GitInfo()
 
@@ -65,7 +65,7 @@ def test_pandas_write_buffer():
 
     csv_url = config.get('global', 'csv_url')
 
-    log_filename = join(log_dir, logfile)
+    log_filename = join(getcwd(), logfile)
     df = pd.read_csv(csv_url)
 
     logger.pandas_write_buffer(df, log_filename)
