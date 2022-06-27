25c788f336009e53ff32313605355cac76f1c50d
Yhan <yhan818@gmail.com>
2022-06-27 12:06:35 -0700

    issue 170. update to python 3.9

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='requiam',
-    version='v1.0.1',
+    version='v1.1.0',
     packages=['requiam'],
     url='https://github.com/UAL-RE/ReQUIAM',
     license='MIT License',
-    author='Chun Ly',
-    author_email='astro.chun@gmail.com',
+    author='Yan Han',
+    author_email='yhan818@gmail.com',
     description='Query EDS information to set EDS attributes specific for Figshare account management',
     long_description=long_description,
     long_description_content_type='text/markdown',
